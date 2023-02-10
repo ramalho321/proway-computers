d4eb5743aa458a9d9832716a1d439825c55aa6db
Breno <breno12ramalho@gmail.com>
2023-02-10 15:05:38 -0300

    build

---
+++ src/app/carrinho.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { IProduto, IProdutoCarrinho } from './produtos';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarrinhoService {
+  itens: IProdutoCarrinho[] = [];
+
+  constructor() { }
+  totalProdutos() {
+    let carrinho = this.obtemCarrinho();
+    let total = 0;
+
+    for (const produto of carrinho) {
+      total += produto.quantidade
+    }
+    return total;
+  }
+  
+  obtemCarrinho() {
+    this.itens = JSON.parse(localStorage.getItem("carrinho") || "[]");
+    return this.itens;
+  }
+
+  adicionarAoCarrinho(produto: IProdutoCarrinho) {
+    this.itens.push(produto);
+    localStorage.setItem("carrinho", JSON.stringify(this.itens));
+  }
+
+  removerProdutoCarrinho(produtoId: number) {
+    this.itens = this.itens.filter(item => item.id !== produtoId)
+    localStorage.setItem("carrinho", JSON.stringify(this.itens));
+  }
+
+  limparCarrinho() {
+    this.itens = [];
+    localStorage.removeItem("carrinho");
+  }
+}
